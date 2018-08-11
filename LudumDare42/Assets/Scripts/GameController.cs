8cde6fa9d574208bd1ec40bcc153c71e4ba41649
EmmaPC\Emma <emmamatthijssen@outlook.com>
2018-08-11 15:11:55 +0200

    + Added a background texture + Added player movement + Added camera scrolling

---
+++ LudumDare42/Assets/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private GameObject _player;
+    private Vector3 velocity = Vector3.zero;
+
+    // Use this for initialization
+    void Start () {
+
+        _player = GameObject.FindGameObjectWithTag("Player");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 pos = Camera.main.WorldToViewportPoint(_player.transform.position);
+
+        Debug.Log(pos);
+
+        if (pos.x < 0.3) MoveCamera(new Vector2(-1, 0));
+        if (0.7 < pos.x) MoveCamera(new Vector2(1, 0));
+        if (pos.y < 0.3) MoveCamera(new Vector2(0, -1));
+        if (0.7 < pos.y) MoveCamera(new Vector2(0, 1));
+
+    }
+
+    void MoveCamera(Vector2 dir)
+    {
+        Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, Camera.main.transform.position + new Vector3(dir.x, dir.y, 0.0f), ref velocity, 0.6f);
+    }
+}
---
+++ LudumDare42/Assets/Scripts/SheepController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D rbd;
+
+	// Use this for initialization
+	void Start () {
+
+        rbd = GetComponent<Rigidbody2D>();
+
+	}
+
+    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+    void FixedUpdate()
+    {
+        //Store the current horizontal input in the float moveHorizontal.
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        //Store the current vertical input in the float moveVertical.
+        float moveVertical = Input.GetAxis("Vertical");
+
+        //Use the two store floats to create a new Vector2 variable movement.
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
+        //rbd.AddForce(movement * speed);
+        rbd.velocity = new Vector3(moveHorizontal * speed, moveVertical * speed);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
